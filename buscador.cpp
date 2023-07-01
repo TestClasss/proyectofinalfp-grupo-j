0a469833df2334e05972be06bf5be9de0de9b7e7
LuisDuque20 <00013423@uca.edu.sv>
2023-07-01 09:14:04 -0600

    implementando fuciones :art:

--- buscador.cpp
+++ buscador.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Vehiculos
+{
+    string idPlaca;
+} vehiculo[];
+
+struct Estacionamiento
+{
+    Vehiculos;
+    bool disponibilidad=true;
+}estacionamiento[50];
+
+
+void IntroducirDatos();
+void BuscadorPlacas();
+
+int main()
+{
+
+    return 0;
+}
+
+void IntroducirDatos()
+{
+}
+
+void BuscadorPlacas()
+{
+}
