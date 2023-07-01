9c7a5bdff357e3a92a1cb5fd0ceb420869d4209e
DiegoCarrillo11 <00076521@uca.edu.sv>
2023-07-01 14:03:41 -0600

    :memo:correccion archivo de texto

--- a.cpp
+++ a.cpp
@@ -2,12 +2,14 @@
 #include <string.h>
 using namespace std;
 void Generararchivodia();
+
 int main()
 {
 Generararchivodia();
 return 0;
 }
 void Generararchivopordia()
+//funcion generadora de archivo por dia
 {string Archivo =to_string(nowLocal.tm_mday)+"-"+to_string(nowLocal.tm_mon+1)+"-"+to_string(nowLocal.tm_year+1900)+".txt"; 
             cout<<Archivo;
             string tipo;
