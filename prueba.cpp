360fb1faae9a4fed8bfa6cb3ea84d355d94c9860
Isaac0405 <00095123@uca.edu.sv>
2023-07-01 14:04:17 -0600

    Borrando bucle :zap:

--- prueba.cpp
+++ prueba.cpp
@@ -1,3 +1,4 @@
+// Función
 void mostrarTicket(double horasParqueo, double costoHora);
 double horasParqueo;
 double costoHora = 0.50;
@@ -5,40 +6,34 @@ int placaVehiculo;
 int op;
 int main()
 {
-    do
-    {
-        cout << "Ingrese la cantidad de tiempo en el parqueo" << endl;
-        cin >> horasParqueo;
-        cout << "Ingrese la placa de su vehiculo" << endl;
-        cin >> placaVehiculo;
-        mostrarTicket(horasParqueo, costoHora);
-        cout << "Desea ingresar otro vehiculo en el parqueo? 0-Si 1-No" << endl;
-        cin >> op;
-    } while (op != 1);
-    return 0;
+     cout << "Ingrese la cantidad de tiempo en el parqueo" << endl;
+     cin >> horasParqueo;
+     mostrarTicket(horasParqueo, costoHora);
+     return 0;
 }
+// Declarar funcion de mostrar tickets
 void mostrarTicket(double horasParqueo, double costoHora)
 {
-    double totalParqueo = horasParqueo * costoHora;
-    cout << "-------------------------------------\n";
-    cout << "\n";
-    cout << "           Ticket del parqueo";
-    cout << "\n"
-         << "\n";
-    cout << "*    Numero de placa";
-    cout << "\n"
-         << "\n";
-    cout << "*    " << placaVehiculo;
-    cout << "\n"
-         << "\n";
-    cout << "*    Tiempo en el parqueo";
-    cout << "\n"
-         << "\n";
-    cout << "*    " << horasParqueo << " horas";
-    cout << "\n"
-         << "\n";
-    cout << "*    Total: $" << totalParqueo ;
-    cout << "\n"
-         << "\n";
-    cout << "-------------------------------------\n";
+     double totalParqueo = horasParqueo * costoHora;
+     cout << "-------------------------------------\n";
+     cout << "\n";
+     cout << "           Ticket del parqueo";
+     cout << "\n"
+          << "\n";
+     cout << "*    Numero de placa";
+     cout << "\n"
+          << "\n";
+     cout << "*    " << placaVehiculo;
+     cout << "\n"
+          << "\n";
+     cout << "*    Tiempo en el parqueo";
+     cout << "\n"
+          << "\n";
+     cout << "*    " << horasParqueo << " horas";
+     cout << "\n"
+          << "\n";
+     cout << "*    Total: $" << totalParqueo;
+     cout << "\n"
+          << "\n";
+     cout << "-------------------------------------\n";
 }
