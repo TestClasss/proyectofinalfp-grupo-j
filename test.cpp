44053d9c36cc10d3eb4754fb2087ef0c15eb4267
LuisDuque20 <00013423@uca.edu.sv>
2023-07-02 22:07:55 -0600

    Resolviendo bug de la funcion :bug:

--- test.cpp
+++ test.cpp
@@ -15,7 +15,7 @@ struct Estacionamiento
   bool disponibilidad=true;
 }estacionamiento[51];
 
-
+void buscadorPlacas();
 
 int main()
 {
@@ -87,6 +87,7 @@ bienvenida:
        case 4:
        break;
        case 5:
+       buscadorPlacas();
        break;
        default:
        cout << "Opcion invalida." << endl;
@@ -96,6 +97,38 @@ bienvenida:
 return 0;
 }
 
+
+
+
+
+void buscadorPlacas()
+{
+
+    string placa; // Ingresa la placa a buscar, es una variable distinta a idPlaca
+    int posicion; // Guardara el valor de la variable "i"
+    cout << "Ingrese la placa del vehiculo que busca: ";
+    cin >> placa;
+    bool encontrado = false;
+    
+    for (int i = 0; i <= 3; i++)
+    {
+        if (placa == vehiculo[i].IdPlaca)
+        {
+            encontrado = true; // Al ingresar los datos de un vehiculo en la posicion "i" el valor de encontrado cambiara
+            posicion = i;
+            break;
+        }
+    }
+
+    if (encontrado)
+    {
+        cout << "Su vehiculo se encuentra en el estacionamiento: " << posicion << endl;
+    }
+    else
+    {
+        cout << "No se encontro su vehiculo" << endl;
+    }
+}
 
 
 
